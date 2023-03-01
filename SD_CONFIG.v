eb50514cdae6c347764fe129fd2cba9d66bc0d06
Chamindu Sauranga <chamindu1567@gmail.com>
2023-03-01 21:22:13 +0530

    Initial IO definition

---
+++ SD_CONFIG.v
@@ -0,0 +1,19 @@
+module (
+    input SDRAM_EN,
+    input CLK,
+    input RST_L,
+    input [3:0] SDRAM_CYCLE,
+    input [3:0] STATE_CNTR,
+    output [11:0] SDRAM_MODE_REG,
+    output [1:0] SDRAM_CMND,
+    output CMND_CYCE_REQ,
+    output SDRAM_SETUP
+);
+
+//Add logic here
+
+
+
+
+
+endmodule
---
+++ SD_REFRESH.v
@@ -0,0 +1,15 @@
+module (
+    input CLK,
+    input RST_L,
+    input SDRAM_SETUP,
+    input [3:0] SDRAM_CYCLE,
+    output RFRSH_REQ
+);
+
+//Add logic here
+
+
+
+
+
+endmodule
